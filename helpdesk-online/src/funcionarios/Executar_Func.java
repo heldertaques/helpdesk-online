7623e17e0e053eb2ad60e612da18ad857f2ce112
expedito_junior947 <expedito_junior947@hotmail.com>
2012-11-16 20:04:50 +0000

    Alterações de Expedito

--- helpdesk-online/src/funcionarios/Executar_Func.java
+++ helpdesk-online/src/funcionarios/Executar_Func.java
@@ -10,11 +10,6 @@ public class Executar_Func {
 	
 	try {
 		
-	
-	f.inserir("carlos", "3349", "8778-8291", "rua do arame","operador universal","","","","","");
-	f.inserir("adriano", "3348", "8778-8291", "rua do arame","operador universal","","","","","");
-	f.inserir("expedito", "3350", "8778-8291", "rua do arame","operador universal","","","","","");
-
 	//Funcionario fun = new Funcionario();
 	
 	//fun = f.pesquisar("3348");
